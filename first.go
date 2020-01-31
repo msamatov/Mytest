0173815f7ca13b2d28adc6dc70af7edd19bc622b
msamatov <musurmanbek.abdisamatov@mail.ru>
2020-01-31 10:51:45 +0600

    real

--- first.go
+++ first.go
@@ -1,5 +1,9 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	fmt.Println("Hello it is my first go project")
+	fmt.Println("opppa onna onarin")
+	fmt.Println("Assa oppa narinariney")
 }
