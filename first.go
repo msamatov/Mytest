45a5d43a78f4fee1de77e72ee53f3277c0736d02
msamatov <musurmanbek.abdisamatov@mail.ru>
2020-01-31 10:52:38 +0600

    realfasdfasfd

--- first.go
+++ first.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello it is my first go project")
 	fmt.Println("opppa onna onarin")
 	fmt.Println("Assa oppa narinariney")
+	fmt.Println("lfasjdl;ajsdfl;jasl;dfjasd")
 }
